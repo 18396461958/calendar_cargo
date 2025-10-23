b346c7a80cc2628eab2d3b55125217ae5666bd36
linjinzhen <2053268305@qq.com>
2025-10-23 19:57:51 +0800

    update README

--- flash.sh
+++ flash.sh
@@ -1,1 +1,3 @@
-sudo stm32flash -w target/thumbv7m-none-eabi/release/calendar.bin -v -g 0x08000000 /dev/ttyCH341USB0 -b 9600
+cargo build --release
+arm-none-eabi-objcopy -O binary -S target/thumbv7m-none-eabi/release/gpio_output target/thumbv7m-none-eabi/release/gpio_output.bin
+sudo stm32flash -w target/thumbv7m-none-eabi/release/gpio_output.bin -v -g 0x08000000 /dev/ttyCH341USB0 -b 9600
